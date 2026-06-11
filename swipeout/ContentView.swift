95b4c096d83871f18824f27f579f7dfe2cefe49f
Paul Jakob <p.jakob-93@web.de>
2026-06-11 19:38:52 +0200

    Initial Commit

---
+++ swipeout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swipeout
+//
+//  Created by Jakob Paul on 11.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swipeout/swipeoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  swipeoutApp.swift
+//  swipeout
+//
+//  Created by Jakob Paul on 11.06.26.
+//
+
+import SwiftUI
+
+@main
+struct swipeoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
